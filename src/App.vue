9ccf023aa14fcc8445b69c914e681f5f3ac072ee
renmin <jimgowork@126.com>
2022-10-26 20:12:20 +0800

    life-cycle

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <h1>msg：{{ msg }}</h1>
+  <div style="margin-bottom: 10px">
+    <button @click="clickMe">更新msg</button>
+  </div>
+  <div>
+    <button @click="noHello">卸载Hello组件</button>
+  </div>
+  <Hello v-if="flag"/>
+</template>
+
+
+<script>
+  import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, ref } from "vue";
+  import Hello from "./Hello.vue";
+  export default {
+    components: {
+      Hello
+    },
+    setup() {
+      const flag = ref(true);
+
+      console.log("setup......");
+      const msg = ref("Life Cycle")
+
+      onBeforeMount(() => {
+        console.log("onBeforeMount......");
+      })
+
+      onMounted(() => {
+        console.log("onMounted......");
+      })
+
+      onBeforeUpdate( ()=> {
+        console.log("onBeforeUpdate......");
+      })
+
+      onUpdated( ()=> {
+        console.log("onUpdated......");
+      })
+
+      const clickMe = () => {
+        msg.value = "Cycle Life "
+      }
+
+      const noHello = () => {
+        flag.value = false;
+      }
+
+      return {
+        msg,
+        clickMe,
+        flag,
+        noHello
+      }
+    },
+  };
+</script>
+
---
+++ src/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello组件</h1>
+</template>
+
+<script>
+  import { onBeforeUnmount, onUnmounted } from "vue";
+  export default {
+    setup() {
+      onBeforeUnmount(() => {
+        console.log("onBeforeUnmount......");
+      });
+      onUnmounted(() => {
+        console.log("onUnmounted......");
+      });
+    },
+  };
+</script>
