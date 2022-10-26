048bd48f51464e672c38827e2ebd5e3151696498
renmin <jimgowork@126.com>
2022-10-26 09:48:41 +0800

    setup函数

---
+++ src/App1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+      <h1 @click="say()">{{ msg }}</h1>
+    </div>
+  </template>
+  <script>
+    export default {
+      setup() {
+        console.log("setup执行了");
+  
+        console.log(this);
+        // 定义数据和函数
+        const msg = "hi vue3";
+        const say = () => {
+          console.log(msg);
+        };
+  
+        return { msg, say };
+      },
+      beforeCreate() {
+        console.log("beforeCreate执行了");
+        console.log(this);
+      },
+    };
+  </script>
+  
